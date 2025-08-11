c8e3826784dc303244a84398938bea62df0c3e1f
Omari <omaradan091@gmail.com>
2025-08-11 23:39:43 +0300

    React state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person:{
+        name:"Omar Oyow",
+        bio:"I am a software engineer with a passion for building web applications",
+        imgSrc:"https://via.placeholder.com/150",
+        Profession: "Software Engineer",
+      },
+      showBio: false,
+      MountedTime:0,
+    };
+    this.Timer = null;
+  }
+
+  componentDidMount() {
+    this.Timer = setInterval(() => {
+      this.setState({MountedTime: this.state.MountedTime + 1});
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.Timer);
+  }
+
+  toggleBio = () => {
+    this.setState({showBio: !this.state.showBio});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>This is a simple React app</p>
+        <button onClick={this.toggleBio}>Toggle Bio</button>
+        {this.state.showBio && <p>{this.state.person.bio}</p>}
+        <p>Mounted Time: {this.state.MountedTime} seconds</p>
+      </div>
+    )
+  }
 }
 
 export default App;
